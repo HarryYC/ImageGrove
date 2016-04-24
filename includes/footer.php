36d552d94adcefade5cd6fdf4eef52147c5a5e63
ycao2 <ycao2@f2101e44-67e3-48d9-836d-95acf9ae7ae6>
2016-04-24 00:44:32 +0000

    move head, header, footer to includes directory

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<div class="grid_3">
+   <div class="container">
+      <ul id="footer-links">
+         <li><a href="#">Terms of Use</a></li>
+         <li><a href="#">Royalty Free License</a></li>
+         <li><a href="#">Extended License</a></li>
+         <li><a href="#">Privacy</a></li>
+         <li><a href="support.php">Support</a></li>
+         <li><a href="about.php">About Us</a></li>
+         <li><a href="faq.php">FAQ</a></li>
+         <li><a href="#">Categories</a></li>
+      </ul>
+   </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,53 @@
+<div class="header">
+   <div class="container">
+      <div class="logo">
+         <h1><a href="index.php">Image Grove</a></h1>
+      </div>
+      <div class="search">
+         <ul class="nav1">
+            <li id="search">
+               <form action="./stock.php" method="get">
+                  <input type="text" name="search_string" id="search_text" placeholder="Search"/>
+                  <input type="button" name="search_button" id="search_button">
+               </form>
+            </li>
+            <li id="options">
+               <a href="#">All Images</a>
+               <ul class="subnav">
+                  <li><a href="#">Images</a></li>
+                  <li><a href="#">Video</a></li>
+               </ul>
+            </li>
+         </ul>
+      </div>
+      <div class="top_right">
+         <ul>
+            <li><a href="register.php">Register</a></li>
+            |
+            <li class="login" >
+               <div id="loginContainer">
+                  <a href="#" id="loginButton"><span>Login</span></a>
+                  <div id="loginBox">
+                     <form id="loginForm">
+                        <fieldset id="body">
+                           <fieldset>
+                              <label for="email">Email Address</label>
+                              <input type="text" name="email" id="email">
+                           </fieldset>
+                           <fieldset>
+                              <label for="password">Password</label>
+                              <input type="password" name="password" id="password">
+                           </fieldset>
+                           <input type="submit" id="login" value="Sign in">
+                           <label for="checkbox"><input type="checkbox" id="checkbox"> <i>Remember me</i></label>
+                        </fieldset>
+                        <span><a href="#">Forgot your password?</a></span>
+                     </form>
+                  </div>
+               </div>
+            </li>
+         </ul>
+      </div>
+      <div class="clearfix"></div>
+   </div>
+</div>
