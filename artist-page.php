68698c964f22c05a37263473e9f26e7648ebf416
earaujo <earaujo@f2101e44-67e3-48d9-836d-95acf9ae7ae6>
2016-04-25 00:33:03 +0000

    removed <head> section from artist-page, added asset includes to include/head.php instead

--- artist-page.php
+++ artist-page.php
@@ -1,29 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- meta stuff from portfolio BS template -->
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Bootstrap Core CSS -->
-        <link href="css/portfolio-css/bootstrap.min.css" rel="stylesheet">
-        <!-- Custom CSS -->
-        <link href="css/portfolio-css/portfolio-item.css" rel="stylesheet">
-        
+        <!-- head includes -->
         <?php
             $page_title = "Artist";
             include("./includes/head.php");
         ?> 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <!--/head-->
+
     <body>
+        <!-- includes header bar with logo, search, register/login etc...-->
         <?php
         $home_page = "false";
         include("./includes/header.php");
--- includes/head.php
+++ includes/head.php
@@ -1,28 +1,35 @@
 <head>
-   <title><?php echo "Image Grove | " . $page_title; ?></title>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-   <meta name="keywords" content="Photo-Hub Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
-      Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
-   <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-   <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-   <!-- Custom Theme files -->
-   <link href="css/style.css" rel='stylesheet' type='text/css' />
-   <!-- Custom Theme files -->
-   <!--webfont-->
-   <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet' type='text/css'>
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
-   <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-   <script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('#horizontalTab').easyResponsiveTabs({
-                    type: 'default', //Types: default, vertical, accordion           
-                    width: 'auto', //auto or any width like 600px
-                    fit: true   // 100% fit in a container
-                });
+    <title><?php echo "Image Grove | " . $page_title; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Photo-Hub Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+          Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- Custom Theme files -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+    <!-- Custom Theme files -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/portfolio-css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/portfolio-css/portfolio-item.css" rel="stylesheet">
+    <!--webfont-->
+    <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#horizontalTab').easyResponsiveTabs({
+                type: 'default', //Types: default, vertical, accordion           
+                width: 'auto', //auto or any width like 600px
+                fit: true   // 100% fit in a container
             });
-        </script>
-   <script src="js/menu_jquery.js"></script>
+        });
+    </script>
+    <script src="js/menu_jquery.js"></script>
 </head>
