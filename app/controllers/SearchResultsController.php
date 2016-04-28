6d5077d6135ad717797e45af97107e2acb4d29b0
mnabril <mnabril@f2101e44-67e3-48d9-836d-95acf9ae7ae6>
2016-04-28 23:13:01 +0000

    Fixed paths for includes on SearchResultsController and Model classes

--- app/controllers/SearchResultsController.php
+++ app/controllers/SearchResultsController.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-include_once '../Models/SearchResultsModel.php';
+include_once '../models/SearchResultsModel.php';
 include_once '../../Product.php';
 
 class SearchResultsController {
--- app/models/Model.php
+++ app/models/Model.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-include './config/dbConnect.php';
+include '../../dbConnect.php';
 
 class Model {
     
