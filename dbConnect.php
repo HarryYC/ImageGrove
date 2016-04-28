24f7e8cb36d87993971d6154c75479bf2bdd4ee0
mdayanan <mdayanan@f2101e44-67e3-48d9-836d-95acf9ae7ae6>
2016-04-28 04:30:14 +0000

    chaging the reference path of config folder

--- dbConnect.php
+++ dbConnect.php
@@ -2,7 +2,7 @@
 function dbConnect() {
     static $conn;
     if (!isset($conn)) {
-        $config = parse_ini_file('C:\xampp\htdocs\PhpProject1\config\config.ini');
+        $config = parse_ini_file(‘..\config\config.ini');
         $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
         if (!$conn) {
             die('Could not connect: ' . mysql_error());
