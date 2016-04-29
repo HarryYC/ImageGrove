b01df460c5f49941452a77890f4f7601a501ec18
ycao2 <ycao2@f2101e44-67e3-48d9-836d-95acf9ae7ae6>
2016-04-29 16:21:26 +0000

    update: style.css, add: .navbar,  form.navbar-form.navbar-left .form-group input.form-control; update: ./includes/header.php, change header style, make it more beautiful;
    create: ./images/logo.png;

--- includes/header.php
+++ includes/header.php
@@ -1,51 +1,50 @@
-<div class="header">
+<nav id="nav-bar" class="navbar navbar-fixed-top" role="navigation">
    <div class="container">
-      <div class="logo">
-         <h1><a href="index.php">Image Grove</a></h1>
-      </div>
-      <?php if($home_page != "true") : ?>
-      <div class="search">
-          <ul class="nav1" style="margin-bottom: 0px;">
-            <li id="search">
-               <form action="./stock.php" method="get">
-                  <input type="text" name="search_string" id="search_text" placeholder="Search"/>
-                  <input type="button" name="search_button" id="search_button">
-               </form>
-            </li>
-<!--            <li id="options">
-               <a href="#">All Images</a>
-               <ul class="subnav">
-                  <li><a href="#">Images</a></li>
-                  <li><a href="#">Video</a></li>
-               </ul>
-            </li>-->
-         </ul>
-      </div>
+   <div class="navbar-header">
+      <a href="index.php"><img class="navbar-brand" src="../images/logo.png"></a>
+   </div>
+   
+   <?php if($home_page != "true") : ?>
+   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-left">
+         <form id="searchForm" class="navbar-form navbar-left" role="search" action="./stock.php" onsubmit="return validateForm()" method="GET">
+            <div class="form-group has-feedback">
+               <div class="input-group">
+                  <input class="form-control" placeholder="Search for photos" type="text" name="search_string" id="searchbar" value="">
+                  <div class="input-group-btn">
+                     <button id="search_button" type="submit" >
+                     <i style="color:white;"></i>
+                     </button>
+                  </div>
+               </div>
+            </div>
+         </form>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
       <?php endif; ?>
       <div class="top_right">
          <ul>
- <?php
-if (!isset($_SESSION['email'])) {
-    echo '            
-                      <li><a href="index.php">Home</a></li>|
-                      <li><a href="#">My Account</a></li>|
-                      <!--<li><a href="register.php">Contact</a></li>|-->
-                      <li><a href="support.php">Support</a></li>|
-                      <li><a href="register.php">Sign in</a></li>
-                      ';
-} else {
-    echo '            
-                      <li><a href="index.php">Home</a></li>|
-                      <li><a href="#">My Account</a></li>|
-                      <!--<li><a href="register.php">Contact</a></li>|-->
-                      <li><a href="support.php">Support</a></li>|
-                      <li><a href="?signout=1">Sign out</a></li>
-                      ';
-}
-?>
+            <?php
+               if (!isset($_SESSION['email'])) {
+                   echo '            
+                                     <li><a href="index.php">Home</a></li>|
+                                     <li><a href="#">My Account</a></li>|
+                                     <!--<li><a href="register.php">Contact</a></li>|-->
+                                     <li><a href="support.php">Support</a></li>|
+                                     <li><a href="register.php">Sign in</a></li>
+                                     ';
+               } else {
+                   echo '            
+                                     <li><a href="index.php">Home</a></li>|
+                                     <li><a href="#">My Account</a></li>|
+                                     <!--<li><a href="register.php">Contact</a></li>|-->
+                                     <li><a href="support.php">Support</a></li>|
+                                     <li><a href="?signout=1">Sign out</a></li>
+                                     ';
+               }
+               echo '</ul></div>';
+               ?>
          </ul>
       </div>
-      <div class="clearfix"></div>
    </div>
-</div>
-                      
+</nav>
