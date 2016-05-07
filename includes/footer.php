2c53a0fa2504f6ad65cfd0cb3291de7f96fc83a7
mnabril <mnabril@f2101e44-67e3-48d9-836d-95acf9ae7ae6>
2016-05-07 00:37:04 +0000

    Fixed category links according to change of filename for search results page.

--- includes/footer.php
+++ includes/footer.php
@@ -7,30 +7,30 @@
             <div class="col-md-9 col_5">
                 <div class="col_1_of_5 span_1_of_5">
                     <ul class="list1">
-                        <li><a href="./stock.php?search_string=Animals">Animals</a></li>
-                        <li><a href="./stock.php?search_string=Fashion">Fashion</a></li>
-                        <li><a href="./stock.php?search_string=Landmarks">Landmarks</a></li>
+                        <li><a href="./search-results.php?search_string=Animals">Animals</a></li>
+                        <li><a href="./search-results.php?search_string=Fashion">Fashion</a></li>
+                        <li><a href="./search-results.php?search_string=Landmarks">Landmarks</a></li>
                     </ul>
                 </div>
                 <div class="col_1_of_5 span_1_of_5">
                     <ul class="list1">
-                        <li><a href="./stock.php?search_string=Celebrities">Celebrities</a></li>
-                        <li><a href="./stock.php?search_string=Holidays">Holidays</a></li>
-                        <li><a href="./stock.php?search_string=Education">Education</a></li>
+                        <li><a href="./search-results.php?search_string=Celebrities">Celebrities</a></li>
+                        <li><a href="./search-results.php?search_string=Holidays">Holidays</a></li>
+                        <li><a href="./search-results.php?search_string=Education">Education</a></li>
                     </ul>
                 </div>
                 <div class="col_1_of_5 span_1_of_5">
                     <ul class="list1">
-                        <li><a href="./stock.php?search_string=Objects">Objects</a></li>
-                        <li><a href="./stock.php?search_string=Miscellaneous">Miscellaneous</a></li>
-                        <li><a href="./stock.php?search_string=Model">Model</a></li>
+                        <li><a href="./search-results.php?search_string=Objects">Objects</a></li>
+                        <li><a href="./search-results.php?search_string=Miscellaneous">Miscellaneous</a></li>
+                        <li><a href="./search-results.php?search_string=Model">Model</a></li>
                     </ul>
                 </div>
                 <div class="col_1_of_5 span_1_of_5">
                     <ul class="list1">
-                        <li><a href="./stock.php?search_string=Nature">Nature</a></li>
-                        <li><a href="./stock.php?search_string=Parks">Parks</a></li>
-                        <li><a href="./stock.php?search_string=Science">Science</a></li>
+                        <li><a href="./search-results.php?search_string=Nature">Nature</a></li>
+                        <li><a href="./search-results.php?search_string=Parks">Parks</a></li>
+                        <li><a href="./search-results.php?search_string=Science">Science</a></li>
                     </ul>
                 </div>
                 <div class="clearfix"></div>
