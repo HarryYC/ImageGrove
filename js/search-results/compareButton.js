11dc2d4074a81474de75cf9397525239b1f490ae
mnabril <mnabril@f2101e44-67e3-48d9-836d-95acf9ae7ae6>
2016-05-17 23:33:32 +0000

    For compare feature, checkboxes become disabled after two are selected. Form button is also disabled until exactly two checkboxes are checked.

--- js/search-results/compareButton.js
+++ js/search-results/compareButton.js
@@ -7,39 +7,10 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
     }
 }
+$("input:checkbox").click(function() {
 
-var limit = 2;
-$('input.single-checkbox').on('change', function (evt) {
-    if ($(this).siblings(':checked').length >= limit) {
-        this.checked = false;
-    }
+  var bol = $("input:checkbox:checked").length >= 2;  
+  $("input:checkbox").not(":checked").attr("disabled",bol);
+  $("#submitBtn").prop("disabled", !bol);
+  
 });
-
-var chks = [];
-getBoxes();
-
-function getBoxes() {
-    var form = document.getElementById("compare");
-    var inputs = form.getElementsByTagName("input");
-
-    for (var i = 0, len = inputs.length; i < len; i++) {
-        if (inputs[i].type == "checkbox")
-            chks.push(inputs[i]);
-        inputs[i].onclick = checkBoxes;
-    }
-}
-
-function checkBoxes() {
-    var count = 0;
-    for (var i = 0, len = chks.length; i < len; i++) {
-        if (chks[i].checked)
-            count++;
-    }
-    if (count === 2) {
-        document.getElementById("submitBtn").disabled = false;
-        return;
-    } else {
-        document.getElementById("submitBtn").disabled = true;
-    }
-}
-
